ea30c862e45865cfdf9838e64ca22c9ed42ba790
ken-sayama <sayamaken0402@gmail.com>
2019-03-14 21:40:54 +0900

    add env

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
+require('dotenv').config();
 const puppeteer = require('puppeteer');
-const email = 'hoge@flagship.cc' // your email
-const password = '0000000' // your password
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://partners.shopify.com/124860');
+  await page.goto(process.env.URL);
   
   // insert your email address
-  await page.type("#account_email", "sayama@flagship.cc");
+  await page.type("#account_email", process.env.EMAIL);
   await page.screenshot({path: 'email.png'});
   page.click('button[type="submit"]');
   await page.waitForNavigation();
 
   // insert your password
-  await page.type("#account_password", "74135841")
+  await page.type("#account_password", process.env.PASSWORD)
   await page.screenshot({path: 'password.png'});
   page.click('button[type="submit"]');
   await page.waitForNavigation();
 
+  await page.screenshot({path: 'shopify-screen.png'});
+
   await browser.close();
 })();
-
